7d50a580f40fe9287fd64e5e1225d2847003c829
Edivaldo António <ediv4ldoantonio@gmail.com>
2024-08-29 23:16:08 +0100

    write documentaion for TodoItem repository

--- Todo.API/Repositories/TodoItemRepository.cs
+++ Todo.API/Repositories/TodoItemRepository.cs
@@ -5,6 +5,9 @@ using Todo.API.Repositories.Contracts;
 
 namespace Todo.API.Repositories;
 
+/// <summary>
+/// A repository that performs CRUD operations on TodoItems.
+/// </summary>
 public class TodoItemRepository : ITodoItemRepository
 {
     private readonly ApplicationDbContext appDbContext;
@@ -14,6 +17,10 @@ public class TodoItemRepository : ITodoItemRepository
         this.appDbContext = appDbContext;
     }
 
+    /// <summary>
+    /// Add a TodoItem to the Database asynchronously.
+    /// </summary>
+    /// <param name="todoItem">The TodoItem to be added.</param>
     public async Task AddAsync(TodoItem todoItem)
     {
         appDbContext.TodoItems.Add(todoItem);
@@ -21,6 +28,11 @@ public class TodoItemRepository : ITodoItemRepository
         await appDbContext.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Delete the specified TodoItem from the Database.
+    /// </summary>
+    /// <param name="id">The Id of the TodoItem to be deleted.</param>
+    /// <exception cref="Exception">Throws when the specified TodoItem does not exist in the Database.</exception>
     public async Task DeleteAsync(string id)
     {
         TodoItem? todoItem = await GetByIdAsync(id)
@@ -31,12 +43,21 @@ public class TodoItemRepository : ITodoItemRepository
         await appDbContext.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Get all the TodoItems that are in the Database.
+    /// </summary>
+    /// <returns>A collection of TodoItems.</returns>
     public async Task<IEnumerable<TodoItem>> GetAllAsync()
     {
         return await appDbContext.TodoItems
             .ToListAsync();
     }
 
+    /// <summary>
+    /// Retrieve the specified TodoItem from the Database.
+    /// </summary>
+    /// <param name="id">The Id of the requested TodoItem</param>
+    /// <returns>The specified TodoItem</returns>
     public async Task<TodoItem?> GetByIdAsync(string id)
     {
         return await appDbContext.TodoItems
@@ -44,6 +65,10 @@ public class TodoItemRepository : ITodoItemRepository
             .FirstOrDefaultAsync();
     }
 
+    /// <summary>
+    /// Updates the specified TodoItem.
+    /// </summary>
+    /// <param name="todoItem">The updated TodoItem</param>
     public async Task UpdateAsync(TodoItem todoItem)
     {
         appDbContext.TodoItems.Update(todoItem);
