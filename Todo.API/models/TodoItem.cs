9b353040b85c636d15ff2160f7fb1ff81fbb90c4
Edivaldo António <ediv4ldoantonio@gmail.com>
2024-08-29 17:44:12 +0100

    Add timestamp fields to TodoItem

--- Todo.API/models/TodoItem.cs
+++ Todo.API/models/TodoItem.cs
@@ -7,15 +7,12 @@ public class TodoItem
 {
     [Key]
     public string Id { get; set; } = Guid.NewGuid().ToString();
-
     [Required]
     public required string Title { get; set; }
-
     public string? Description { get; set; }
-
     public DateTime DueDate { get; set; }
-
     public Priority Priority { get; set; }
-
     public Status Status { get; set; } = Status.Pending;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
 }
